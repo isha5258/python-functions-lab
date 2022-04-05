43d2527f526d3eda8b3e2430bb5de7bff0bf9436
IsaacH <Isaac.hathaway28@gmail.com>
2022-04-05 16:29:08 -0600

    complet exercises 2, 3, and 4

--- python-functions.py
+++ python-functions.py
@@ -9,8 +9,21 @@ def sum_to(n):
 
 # 2 Write a function named largest that takes a list of numbers as an argument and returns the largest number in that list.
 
+def largest(nums):
+  return max(nums)
+largest([10, 4, 2, 231, 91, 54])
 
 
 #  3 Write a function named occurrences that takes two string arguments as input and counts the number of occurrences of the second string inside the first string.
 
+def occurrances(str1, str2):
+  return str1.count(str2)
+
+
 # 4 Write a function named product that takes an arbitrary number of numbers, multiplies them all together, and returns the product. HINT: Review your notes on args.
+
+def product(*args):
+  total = 1
+  for num in args:
+    total *= num
+  return total
